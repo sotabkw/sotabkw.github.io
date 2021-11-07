beec23d642561ae84a435bf297f75173f12cabae
watanabe-sota <819sota@gmail.com>
2021-11-07 13:37:50 +0900

    fix:app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,18 @@ import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />{' '}
+      <style jsx global>{`
+        body {
+          font-family: 'Spinnaker';
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
+    </div>
+  )
 }
 export default MyApp
