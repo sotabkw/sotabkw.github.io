75135068e2c5801372df57b558c485ca10b4fa60
watanabe-sota <819sota@gmail.com>
2021-11-07 13:36:10 +0900

    add: icon

---
+++ src/components/uiParts/Icon/activity.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const ActivityIcon: React.VFC = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="feather feather-activity"
+    >
+      <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
+    </svg>
+  )
+}
