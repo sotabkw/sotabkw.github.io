38dfb4a490566e02f714f1d1a0ad2b0691116337
sotabkw <sotabkw@users.noreply.github.com>
2022-06-21 15:33:53 +0000

    deploy: ec62131882b739317e3cbeef8bf38e62f1761623

---
+++ _next/static/L26hg4bTWRJlQ2Yp56702/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/152-97809a7f98e28985.js","static/chunks/pages/index-f3a610cd5d6f710e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
